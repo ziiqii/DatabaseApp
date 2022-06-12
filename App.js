a45a6230d2c2df25ec2b8eb04f719812d713513f
Ziqi <ziiiqiii@gmail.com>
2022-06-12 22:08:30 +0800

    Added styling

--- App.js
+++ App.js
@@ -1,31 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 function NotesScreen() {
   return (
     <View style={styles.container}>
       <Text>Notes</Text>
     </View>
-  )
+  );
 }
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>A</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            title: "Todo",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              fontSize: 24,
+            },
+            headerStyle: {
+              backgroundColor: "orange",
+              height: 120,
+              borderBottomColor: "#999",
+              borderBottomWidth: 2,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
