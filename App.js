caabcd0eb9e82daee228e702a129c77b394a5dcd
Ziqi <ziiiqiii@gmail.com>
2022-06-12 21:42:44 +0800

    Fixes imports

--- App.js
+++ App.js
@@ -2,6 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+function NotesScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Notes</Text>
+    </View>
+  )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
