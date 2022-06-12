d6070b75cdbcbcedb5f795719ba371456a7591d4
Ziqi <ziiiqiii@gmail.com>
2022-06-12 21:38:39 +0800

    Fixed imports

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
