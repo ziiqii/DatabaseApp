cc9fd3d57ea829ec1f21db4976af01ad0253aa3c
Ziqi <ziiiqiii@gmail.com>
2022-06-12 22:25:26 +0800

    Added SQLite

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("notes.db");
 
 function NotesScreen({ navigation }) {
   useEffect(() => {
